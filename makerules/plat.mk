72f8f75122eb93b36cc61abf95f0c078f47712c5
lihuibin <lihuibin705@163.com>
2019-07-17 11:25:02 +0800

    For system: 编译链接添加 libz 和 pthread库连接

--- makerules/plat.mk
+++ makerules/plat.mk
@@ -1,7 +1,9 @@
 PLAT_ASFLAGA = __USE_GNU
-PLAT_CCFLAGS = -DCONFIG_TARGET_CPU_${TARGET_CPU} -D_GNU_SOURCE -std=c++11 -Wall -Wno-unused -Werror -O3
+PLAT_CCFLAGS = -DCONFIG_TARGET_CPU_${TARGET_CPU} -D_GNU_SOURCE -std=c++11 -Wall -Wno-unused -Werror -O3 
+
 
 PLAT_LDFLAGS = -L${PORTING_INSTALL_DIR}/lib \
+			   -lz -lpthread
 
 
 
